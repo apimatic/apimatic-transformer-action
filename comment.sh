ebe0e43fbb5290c997028799031edf868fcd1383
Mujtaba Mehdi <13besemmehdi@seecs.edu.pk>
2020-11-23 23:26:29 +0500

    Update comment.sh

--- comment.sh
+++ comment.sh
@@ -8,7 +8,7 @@ somevar1=$(curl -X POST \
   --header ${INPUT_AUTH_ID} \
   -H 'Accept: application/json'\
   -H 'content-type: application/vnd.apimatic.urlTransformDto.v1+json' \
-  --data @transform.json | jshon -e generatedFile | sed -e 's/^"//' -e 's/"$//')
+  --data @transform.json | jq '.generatedFile' | sed -e 's/^"//' -e 's/"$//')
 
 echo $somevar1
 download_path1="https://staging.apimatic.io/${somevar1}"
