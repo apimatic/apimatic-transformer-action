c68f979c9ce8f81a77c1f90ac535cc7cb3286194
Mujtaba Mehdi <13besemmehdi@seecs.edu.pk>
2020-11-23 22:36:29 +0500

    Update comment.sh

--- comment.sh
+++ comment.sh
@@ -1,11 +1,11 @@
-cd Transformations
+#cd Transformations
 uname=$(date +'%m-%d-%Y'+"%H:%M")
 
 
 
 somevar1=$(curl -X POST \
   --url 'https://staging.apimatic.io/api/transformations' \
-  --header $1 \
+  --header ${INPUT_AUTH_ID} \
   -H 'Accept: application/json'\
   -H 'content-type: application/vnd.apimatic.urlTransformDto.v1+json' \
   --data @transform.json | jshon -e generatedFile | sed -e 's/^"//' -e 's/"$//')
@@ -14,15 +14,11 @@ echo $somevar1
 download_path1="https://staging.apimatic.io/${somevar1}"
 echo $download_path1
 name1=petapi_$uname.json
-curl --location --header $1 --remote-header-name -o $name1 $download_path1
+#curl --location --header ${INPUT_AUTH_ID} --remote-header-name -o $name1 $download_path1
 
 
 
 
 
 
-git config user.name "mujjazi"
-git config user.email "13besemmehdi@seecs.edu.pk"
-git add .
-git commit -m 'Downloaded Transformation'
-git push
+
