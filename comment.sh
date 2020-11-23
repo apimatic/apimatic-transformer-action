73437e3753b07532ff81b66c322c6ec75735bb25
Mujtaba Mehdi <13besemmehdi@seecs.edu.pk>
2020-11-23 23:59:58 +0500

    Update comment.sh

--- comment.sh
+++ comment.sh
@@ -1,9 +1,11 @@
 #cd Transformations
 uname=$(date +'%m-%d-%Y'+"%H:%M")
 
+user_auth=$1
+
 somevar1=$(curl -X POST \
   --url 'https://staging.apimatic.io/api/transformations' \
-  -H 'Authorization: Basic ZGV2ZWxvcGVyQGFwaW1hdGljLmlvOndsY3VtTnU4' \
+  -H $user_auth \
   -H 'Accept: application/json'\
   -H 'content-type: application/vnd.apimatic.urlTransformDto.v1+json' \
   --data @transform.json | jq '.generatedFile' | sed -e 's/^"//' -e 's/"$//')
