e21ce953c4f75eabd5e94ec1ef021197e6a2106c
Mujtaba Mehdi <13besemmehdi@seecs.edu.pk>
2020-11-24 00:30:19 +0500

    Update comment.sh

--- comment.sh
+++ comment.sh
@@ -2,14 +2,6 @@
 uname=$(date +'%m-%d-%Y'+"%H:%M")
 
 user_auth=$1
-echo $user_auth
-
-curl -X POST \
-  --url 'https://staging.apimatic.io/api/transformations' \
-  -H "$user_auth" \
-  -H 'Accept: application/json'\
-  -H 'content-type: application/vnd.apimatic.urlTransformDto.v1+json' \
-  --data @transform.json | jq '.generatedFile' | sed -e 's/^"//' -e 's/"$//' 
 
 somevar1=$(curl -X POST \
   --url 'https://staging.apimatic.io/api/transformations' \
@@ -21,8 +13,8 @@ somevar1=$(curl -X POST \
 echo $somevar1
 download_path1="https://staging.apimatic.io/${somevar1}"
 echo $download_path1
-name1=petapi_$uname.json
-#curl --location --header ${INPUT_AUTH_ID} --remote-header-name -o $name1 $download_path1
+
+echo "::set-output name=specurl::$download_path1"
 
 
 
