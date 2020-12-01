af8a28c1c788c6376b5acdcb7f853813b3bdfb1b
Mujtaba Mehdi <13besemmehdi@seecs.edu.pk>
2020-12-01 19:47:31 +0500

    Update comment.sh

--- comment.sh
+++ comment.sh
@@ -2,6 +2,16 @@
 user_auth=$1
 input=$2
 export=$3
+
+#Trimmed Arguments
+trimmed_auth=$(echo user_auth | xargs) 
+trimmed_input=$(echo input | xargs)
+trimmed_auth=$(echo export | xargs)
+
+echo $trimmed_auth
+echo $trimmed_input
+echo $trimmed_export
+
 file=0
 FILE=transform.json
 
@@ -16,19 +26,19 @@ if [[ $file -gt 0 ]]
 then
   transformedfile=$(curl -X POST \
   --url 'https://www.apimatic.io/api/transformations' \
-  -H "$user_auth" \
+  -H "$trimmed_auth" \
   -H 'Accept: application/json'\
   -H 'content-type: application/vnd.apimatic.urlTransformDto.v1+json' \
   --data @transform.json | jq '.generatedFile' | sed -e 's/^"//' -e 's/"$//')
 else
   transformedfile=$(curl -X POST \
   --url 'https://www.apimatic.io/api/transformations' \
-  -H "$user_auth" \
+  -H "$trimmed_auth" \
   -H 'Accept: application/json'\
   -H 'content-type: application/vnd.apimatic.urlTransformDto.v1+json' \
   --data '{
-  "fileUrl": "'"${input}"'",
-  "exportFormat": "'"${export}"'",
+  "fileUrl": "'"${trimmed_input}"'",
+  "exportFormat": "'"${trimmed_export}"'",
   "codeGenVersion": 1
   }' | jq '.generatedFile' | sed -e 's/^"//' -e 's/"$//')
 fi
