4e80c3513ae3427f88cb07a887fe2dd927a0f074
Mujtaba Mehdi <13besemmehdi@seecs.edu.pk>
2020-12-02 18:04:14 +0500

    Update comment.sh

--- comment.sh
+++ comment.sh
@@ -19,15 +19,15 @@ else
     file=-1
 fi
 
-curl -X POST -LI https://www.apimatic.io/api/transformations -H "$auth1" \
+curl -LI https://www.apimatic.io/api/transformations -H "$auth1"  -o /dev/null -w '%{http_code}\n' -s \
   -H 'Accept: application/json'\
   -H 'content-type: application/vnd.apimatic.urlTransformDto.v1+json' \
   --data '{
   "fileUrl": "'"${input1}"'",
   "exportFormat": "'"${export1}"'",
   "codeGenVersion": 1
-  }' \ 
-  -o /dev/null -w '%{http_code}\n' -s
+  }' 
+ 
 echo $http_code
 
 
@@ -52,22 +52,10 @@ else
   }' | jq '.generatedFile' | sed -e 's/^"//' -e 's/"$//')
 fi
 
-download_path2="You have an error in your authorization token or InputURL/Export Format!"
 
-teststring=literal
-echo $transformedfile
-
-
-if echo $teststring | grep -q $transformedfile ; then echo "found"; else echo "not found"; fi
-
-if [[ ${transfomedfile} == *"literal"* ]]; 
-then
-  echo "You have an error in your authorization token or InputURL/Export Format!"
-  echo "::set-output name=specurl::$download_path2"  
-else
   download_path1="https://www.apimatic.io${transformedfile}"
   echo "::set-output name=specurl::$download_path1"
-fi
+
 
 
 
